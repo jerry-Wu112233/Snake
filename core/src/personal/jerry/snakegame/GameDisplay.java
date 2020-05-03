04be952bfcdae2f511ed407aac8a742d74e2d5d7
jerry-Wu112233 <yihanwu4@illinois.edu>
2020-05-03 14:14:44 -0500

    Implemented methods within GameDisplay

--- core/src/personal/jerry/snakegame/GameDisplay.java
+++ core/src/personal/jerry/snakegame/GameDisplay.java
@@ -3,11 +3,23 @@ package personal.jerry.snakegame;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import jdk.javadoc.internal.tool.Main;
 
 public class GameDisplay implements Screen {
-    private Main activity;
-
+    private MainGame activity;
+    private int width = 600;
+    private int height = 1000;
+    private OrthographicCamera cam = new OrthographicCamera(width, height);
+    private Viewport view;
+    public GameDisplay(MainGame game) {
+        this.activity = game;
+        cam.setToOrtho(false, width, height);
+        view = new FitViewport(width, height, cam);
+        view.apply();
+    }
     @Override
     public void show() {
 
--- core/src/personal/jerry/snakegame/MainGame.java
+++ core/src/personal/jerry/snakegame/MainGame.java
@@ -1,15 +1,11 @@
 package personal.jerry.snakegame;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MainGame extends Game {
 	SpriteBatch batch;
-	Texture img;
 	@Override
 	public void render() {
 		super.render();
@@ -19,9 +15,6 @@ public class MainGame extends Game {
 		batch = new SpriteBatch();
 		this.setScreen(new GameDisplay(this));
 	}
-
-
-	
 	@Override
 	public void dispose () {
 		batch.dispose();
